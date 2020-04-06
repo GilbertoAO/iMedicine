b781df1fb5398494524065d40f779e08b6c1f5dc
GilbertoAO <gilbertoaoliveirajr@gmail.com>
2020-04-06 14:38:01 -0300

    Primeiro commit

---
+++ armazenamento.py
@@ -0,0 +1,14 @@
+import shelve   
+from modelo import Remedio
+def salva(remedio):
+    arquivo = shelve.open("remedios")
+    arquivo[remedio.nome] = remedio
+    arquivo.close
+
+def abre():
+    arquivo = shelve.open("remedios")
+    lista_remedios = list(arquivo.keys())
+    remedios = [arquivo[remedio] for remedio in lista_remedios]
+    arquivo.close
+    return remedios 
+
---
+++ interface.py
@@ -0,0 +1,80 @@
+from modelo import Remedio, ListaRemedios
+from armazenamento import abre, salva
+
+def carrega():
+    return abre()
+def input_inicio():
+    print("###################################################")
+    print("############ Menu 1: O que deseja fazer? ##########")
+    print("###################################################")
+    print("1 - Cadastrar um novo remédio")
+    print("2 - Consultar informações de remédio cadastrado") 
+    print("3 - Confirmar que tomou")
+    print("4 - encerrar")
+    controle = input()
+    return controle
+
+def NovoRemedio():
+    print("Cadastro de novo remédio")
+    nome_remedio = input("Nome do remédio: ")
+    quantidade = input("dose: ")
+    periodo = input("Tomar remédio a cada quantas horas?")
+    novo_remedio = Remedio(nome_remedio, quantidade, periodo)
+    print(novo_remedio)
+    salva(novo_remedio)
+    input("aperte enter para continuar")
+    return novo_remedio
+
+def MostraRemedio():
+    print("escolha uma opção:")
+    todos_remedios = carrega()
+    print(ListaRemedios(todos_remedios))
+    controle = int(input())
+
+    return todos_remedios[controle-1]
+
+
+def tomou():
+
+    print("informar que tomou remédio. ")
+    remedio = MostraRemedio()
+    remedio.toma()
+    salva(remedio)
+    input("Informação registrada. Enter para continuar...")
+
+def verifica_se_tomou():
+    remedio = MostraRemedio()
+    if remedio.verifica_se_tomou_hoje:
+        print(f"Você já tomou o remédio hoje: {remedio.ultima_vez_que_tomou}")
+    else:
+        print(f"Você ainda não tomou o remédio hoje:  {remedio.ultima_vez_que_tomou}")
+    
+
+def rotina_inicial():
+    rodando = True
+    while rodando:
+        controle = input_inicio()
+        if controle == "1":
+            NovoRemedio()   
+        elif controle == "2":
+            print("###################################################")
+            print("############ Menu 1: O que deseja fazer? ##########")
+            print("###################################################")
+            print("1 - Verificar se tomou")
+            print("2 - Apenas visualizar informações")
+            control2 = input()
+            if control2 == '1':
+                verifica_se_tomou()
+            if control2 == '2':
+                
+                print(MostraRemedio())
+                input("aperte enter para continuar...")                
+        elif controle == "3":
+            tomou()
+        elif controle == "4":
+            rodando = False
+
+
+if(__name__ == "__main__"):
+    rotina_inicial()
+
